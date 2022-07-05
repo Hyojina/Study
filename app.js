3200435cf4655076cdf15d9a06348bd6769eb993
Hyojin Kim <hjkim3009@gmail.com>
2022-07-05 16:54:20 +0900

    const, let and var

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const a = 5;
 const b = 2;
-const myName = "hyojin";
+let myName = "hyojin";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+myName = "jinhyo";
+
+console.log("your new name is " + myName);
+
+// 이렇게 쓰기도 합니다.
+// console.log(`your new name is ${myName}`);
