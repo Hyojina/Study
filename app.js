f81ad5d0fc7720af28cfd7f49fb4137dfdcc75fa
Hyojin Kim <hjkim3009@gmail.com>
2022-07-05 19:09:52 +0900

    Booleans

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-const a = 5;
-const b = 2;
-let myName = "hyojin";
+const a = null;
+let something;
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "jinhyo";
-
-console.log("your new name is " + myName);
-
-// 이렇게 쓰기도 합니다.
-// console.log(`your new name is ${myName}`);
+console.log(something);
