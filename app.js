56dc75c767342d68594b03e7c76f568ce76b14b3
Hyojin Kim <hjkim3009@gmail.com>
2022-07-05 19:57:48 +0900

    Arrays

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-const a = null;
-let something;
+// const형 배열은 배열내 존재하는 값들을 수정할 수 있고 제거도 가능하다.
+// 이유는 상수 배열은 값 자체를 접근하는 것이 아닌 참조값으로 접근하고 있기 때문.
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"]; //array
 
-console.log(something);
+// Get item from array
+console.log(daysOfWeek);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
