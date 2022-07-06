fbc420da0fa43439c7c8302eaac4f7b0de2e72b2
Hyojin Kim <hjkim3009@gmail.com>
2022-07-06 16:25:45 +0900

    Conditional

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-const calculator = {
-  add: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  devide: function (a, b) {
-    return a / b;
-  },
-  squared: function (a, b) {
-    return a ** b;
-  },
-};
+const age = parseInt(prompt("How old are you?"));
 
-const addResult = calculator.add(7, 6);
-const minusResult = calculator.minus(addResult, 4);
-const devideResult = calculator.devide(8, minusResult);
-const squaredResult = calculator.squared(addResult, devideResult);
-
-console.log(addResult);
-console.log(minusResult);
-console.log(devideResult);
-console.log(squaredResult);
+console.log(age);
