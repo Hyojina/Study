8d69b3d4a7bd82498533a21fd486b1d8b22f11fb
Hyojin Kim <hjkim3009@gmail.com>
2022-07-06 14:48:50 +0900

    Calcuator

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const player = {
-  name: "Hyojin",
-  sayHello: function (otherPersonsName) {
-    console.log("Hello! " + otherPersonsName + " nice to meet you!");
+const calculator = {
+  add: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  devide: function (a, b) {
+    console.log(a / b);
+  },
+  squared: function (a, b) {
+    console.log(a ** b);
   },
 };
 
-console.log(player.name);
-player.sayHello("Jinhyo");
+calculator.add(7, 6);
+calculator.minus(10, 4);
+calculator.devide(8, 2);
+calculator.squared(5, 3);
