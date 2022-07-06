959b999a06ea9fa874ec8fd2b0645a5bb36e42f9
Hyojin Kim <hjkim3009@gmail.com>
2022-07-06 13:08:33 +0900

    Function Part.1

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-const player = {
-  // property 특성 적기
-  name: "Hyojin",
-  points: 100,
-  fat: false,
-};
+function sayHello() {
+  console.log("Hello!");
+}
 
-console.log(player);
-
-player.name = "Jinhyo";
-player.points = player.points + 20;
-
-console.log(player.name);
-console.log(player["name"]);
-console.log(player.points);
-console.log(player["points"]);
+sayHello();
+sayHello();
+sayHello();
