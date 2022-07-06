d5787a8ccaf918c2f3a849a6da5d8f2dbe9e3a32
Hyojin Kim <hjkim3009@gmail.com>
2022-07-06 12:46:42 +0900

    Objects_2

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-// 하나의 개체, 즉 player에 대한 설명을 하고싶다면 objects를 만드는 것이 좋다.
-
-// 변수로 선언하는 것이 적합하지 않은 이유는 모든 변수의 이름을 다 기억할 수 없다.
-const playerNameX = "Hyojin";
-const playerPointsX = 121212;
-const playerBeautyX = true;
-
-// array가 적합하지 않은 이유는 배열값이 어떤 의미인지 알 수 없기 때문
-const playerX = ["Hyojin", 121212, true];
-
-
-const player ={
-    // property 특성 적기
-    name: "Hyojin",
-    points: 100,
-    fat: false,
+const player = {
+  // property 특성 적기
+  name: "Hyojin",
+  points: 100,
+  fat: false,
 };
 
 console.log(player);
+
+player.name = "Jinhyo";
+player.points = player.points + 20;
+
 console.log(player.name);
-console.log(player["name"]); //위와 동일함
+console.log(player["name"]);
+console.log(player.points);
+console.log(player["points"]);
