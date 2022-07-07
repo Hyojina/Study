a898e3baf83d649b9ef7b5f795f57d92c4ea420b
Hyojin Kim <hjkim3009@gmail.com>
2022-07-07 21:54:33 +0900

    Event Part.2

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const title = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick() {
   console.log("title was clicked!");
+  title.innerText = "CLICKED!";
+  title.style.color = "green";
+}
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+  title.style.color = "blue";
+}
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
   title.style.color = "red";
 }
 
 //자바스크립트가 실행할거니까 함수에 () 붙여서 실행해버리지 않기! ()는 실행하는거임!
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
