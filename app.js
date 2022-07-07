415d3eef802eb9859940f7df8f34cc3e8b963006
Hyojin Kim <hjkim3009@gmail.com>
2022-07-07 21:40:37 +0900

    Event Part.1

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const title = document.querySelector(".hello h1");
+const title = document.querySelector(".hello:first-child h1");
 
-title.innerText = "Hello";
+function handleTitleClick() {
+  console.log("title was clicked!");
+  title.style.color = "red";
+}
+
+//자바스크립트가 실행할거니까 함수에 () 붙여서 실행해버리지 않기! ()는 실행하는거임!
+title.addEventListener("click", handleTitleClick);
