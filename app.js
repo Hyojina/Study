b58ff89c9b7839a8e850120d0bbb3dd58cf4aab5
Hyojin Kim <hjkim3009@gmail.com>
2022-07-07 22:37:31 +0900

    Event Part.3

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
-const title = document.querySelector(".hello:first-child h1");
+const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  console.log("title was clicked!");
-  title.innerText = "CLICKED!";
-  title.style.color = "green";
+  console.log("Title was clicked!");
+  h1.innerText = "CLICKED!";
+  h1.style.color = "green";
 }
 function handleMouseEnter() {
-  title.innerText = "Mouse is here!";
-  title.style.color = "blue";
+  h1.innerText = "Mouse is here!";
+  h1.style.color = "blue";
 }
 function handleMouseLeave() {
-  title.innerText = "Mouse is gone!";
-  title.style.color = "red";
+  h1.innerText = "Mouse is gone!";
+  h1.style.color = "white";
 }
 
-//자바스크립트가 실행할거니까 함수에 () 붙여서 실행해버리지 않기! ()는 실행하는거임!
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+function handleWindowResize() {
+  document.body.style.backgroundColor = "gray";
+}
+function handleWindowCopy() {
+  alert("copier!");
+  h1.innerText = "COPIER!!";
+  h1.style.color = "red";
+}
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+function handleWindowOnline() {
+  alert("ALL GOOD");
+}
+
+//자바스크립트가 실행할거니까 함수에 () 붙여서 실행해버리지 않기! ()는 실행하는거임! h1.addEventListener("click", handleTitleClick); h1.addEventListener("mouseenter", handleMouseEnter); h1.addEventListener("mouseleave", handleMouseLeave);
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
