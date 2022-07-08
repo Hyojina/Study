c5d49dbfadb1627dab8452920509ce0ed6c60427
Hyojin Kim <hjkim3009@gmail.com>
2022-07-08 16:23:19 +0900

    CSS in Javascript Part.3-2

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  const clickedClass = "clicked";
-  if (h1.classList.contains(clickedClass)) {
-    h1.classList.remove(clickedClass);
-  } else {
-    h1.classList.add(clickedClass);
-  }
-  // 이렇게 하면 섹시폰트는 살아있다!
+  h1.classList.toggle("clicked");
+  // 이렇게 하면 섹시폰트는 살아있다! 그리고 아주 간단한 코드!
 }
 
 h1.addEventListener("click", handleTitleClick);
