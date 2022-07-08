0c4c3a10721be81eba9037acb412e982e6920007
Hyojin Kim <hjkim3009@gmail.com>
2022-07-08 14:10:18 +0900

    CSS in Javascript

--- app.js
+++ app.js
@@ -1,40 +1,15 @@
-const h1 = document.querySelector(".hello h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  console.log("Title was clicked!");
-  h1.innerText = "CLICKED!";
-  h1.style.color = "green";
-}
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here!";
-  h1.style.color = "blue";
-}
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-  h1.style.color = "white";
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "plum") {
+    newColor = "tomato";
+  } else {
+    newColor = "plum";
+  }
+  h1.style.color = newColor;
+  console.log(currentColor);
 }
 
-function handleWindowResize() {
-  document.body.style.backgroundColor = "gray";
-}
-function handleWindowCopy() {
-  alert("copier!");
-  h1.innerText = "COPIER!!";
-  h1.style.color = "red";
-}
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-function handleWindowOnline() {
-  alert("ALL GOOD");
-}
-
-//자바스크립트가 실행할거니까 함수에 () 붙여서 실행해버리지 않기! ()는 실행하는거임! h1.addEventListener("click", handleTitleClick); h1.addEventListener("mouseenter", handleMouseEnter); h1.addEventListener("mouseleave", handleMouseLeave);
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
