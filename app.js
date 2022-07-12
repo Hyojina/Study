cd37fab09311c2363edda525ceda945c3b3234f8
Hyojin Kim <hjkim3009@gmail.com>
2022-07-12 19:18:04 +0900

    Events Part.2

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 //현재 button이 사라져서 작동하지 않음! 대신 이전의 function 부분을 html에서 구현!
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
 
+//이벤트 정보가 넘어온다!!
 function onLoginSubmit(event) {
-  //이벤트 정보가 넘어온다!!
-
   event.preventDefault(); //어떤 event의 기본행동이든지 발생되지 않도록 막음!!
   console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.log(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
