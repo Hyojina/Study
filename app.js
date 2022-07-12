5975479a8b89b9ae9fb9d9b70e18754d4a1d37d2
Hyojin Kim <hjkim3009@gmail.com>
2022-07-12 20:03:11 +0900

    Saving Username

--- app.js
+++ app.js
@@ -2,21 +2,19 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-//두번 이상 사용하는 값들은 변수로 만드는 것이 좋다.
-//그리고 일반적으로 string만 포함된 변수는 대문자로 표기함!
 const HIDDEN_CLASSNAME = "hidden";
 
-//이벤트 정보가 넘어온다!!
 function onLoginSubmit(event) {
-  event.preventDefault(); //어떤 event의 기본행동이든지 발생되지 않도록 막음!!
+  event.preventDefault();
   const username = loginInput.value;
 
-  console.log(username);
-  //greeting.innerText = "Hello " + username;
   greeting.innerText = `Hello ${username}!`;
 
   greeting.classList.remove(HIDDEN_CLASSNAME);
   loginForm.classList.add(HIDDEN_CLASSNAME);
+
+  localStorage.setItem("username", username);
+  console.log(localStorage.getItem("username"));
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
