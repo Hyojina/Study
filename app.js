4008a0c7d7cbea3fd50dd87a2deba5bbb3e73fbf
Hyojin Kim <hjkim3009@gmail.com>
2022-07-12 18:18:52 +0900

    Form Submission 2

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
+//현재 button이 사라져서 작동하지 않음! 대신 이전의 function 부분을 html에서 구현!
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
   const username = loginInput.value;
-  if (username === "") {
-    alert("Please write your name");
-  } else if (username.length > 15) {
-    alert("Your name is too long");
-  }
+  console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
