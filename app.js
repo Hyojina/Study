b1923e50ac1dd594cd01cb72c0198b9d90e17c46
Hyojin Kim <hjkim3009@gmail.com>
2022-07-12 19:45:35 +0900

    Getting Username

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-//현재 button이 사라져서 작동하지 않음! 대신 이전의 function 부분을 html에서 구현!
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+//두번 이상 사용하는 값들은 변수로 만드는 것이 좋다.
+//그리고 일반적으로 string만 포함된 변수는 대문자로 표기함!
+const HIDDEN_CLASSNAME = "hidden";
 
 //이벤트 정보가 넘어온다!!
 function onLoginSubmit(event) {
   event.preventDefault(); //어떤 event의 기본행동이든지 발생되지 않도록 막음!!
-  console.log(loginInput.value);
-}
+  const username = loginInput.value;
 
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.log(event);
+  console.log(username);
+  //greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}!`;
+
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
