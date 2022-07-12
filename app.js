416d1cc8e83a0acc7fa1c3295712f13f6f20ae67
Hyojin Kim <hjkim3009@gmail.com>
2022-07-12 18:12:44 +0900

    Form Submission

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
-// const loginInput = document.querySelector("#login-form input");
-// const loginButton = document.querySelector("#login-form button");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-  console.log(loginInput.value);
+  const username = loginInput.value;
+  if (username === "") {
+    alert("Please write your name");
+  } else if (username.length > 15) {
+    alert("Your name is too long");
+  }
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
