2d127d5ccf2e3dd18e9958e7e97fdbe51bd533bb
Hyojin Kim <hjkim3009@gmail.com>
2022-07-13 22:00:42 +0900

    PadStart

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,12 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
   const date = new Date();
-  clock.innerText = `${date.getFullYear()}년 ${date.getMonth()}월 ${date.getDate()}일 ${date.getHours()}시 ${date.getMinutes()}분 ${date.getSeconds()}초`;
+
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+
+  clock.innerText = `${date.getFullYear()}년 ${date.getMonth()}월 ${date.getDate()}일 ${hours}시 ${minutes}분 ${seconds}초`;
 }
 
 getClock(); //이걸 써주면 1초를 안기다려도 처음에 보여줌!!
