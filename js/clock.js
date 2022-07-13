fc5347f6ea07f267cf1963989c5c3a9eb7bb6d71
Hyojin Kim <hjkim3009@gmail.com>
2022-07-13 22:48:05 +0900

    PadStart 2

--- js/clock.js
+++ js/clock.js
@@ -3,9 +3,9 @@ const clock = document.querySelector("h2#clock");
 function getClock() {
   const date = new Date();
 
-  const hours = date.getHours().toString().padStart(2, "0");
-  const minutes = date.getMinutes().toString().padStart(2, "0");
-  const seconds = date.getSeconds().toString().padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
 
   clock.innerText = `${date.getFullYear()}년 ${date.getMonth()}월 ${date.getDate()}일 ${hours}시 ${minutes}분 ${seconds}초`;
 }
