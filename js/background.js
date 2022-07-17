d4d66355743179b9775133c91fe81eb78f87193e
Hyojin Kim <hjkim3009@gmail.com>
2022-07-17 15:13:36 +0900

    Background

---
+++ js/background.js
@@ -0,0 +1,10 @@
+//img 폴더의 이미지 파일들 이름과 동일하게
+const images = ["0.jpg", "1.jpg", "2.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
