575910147574ce5d2b001c379b9ce6d1a77ce4fc
Hyojin Kim <hjkim3009@gmail.com>
2022-07-23 22:38:44 +0900

    My First Drawing

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("hello!");
+const canvas = document.querySelector("canvas");
+
+// 캔버스에 그림 그릴 때 쓰는거
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 800;
+
+//x좌표,y좌표,가로길이,세로길이
+ctx.fillRect(50, 50, 100, 200);
