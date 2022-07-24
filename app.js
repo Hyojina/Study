7a2f99a9137a05a4e713e48394ff3aee23833b83
Hyojin Kim <hjkim3009@gmail.com>
2022-07-24 22:29:53 +0900

    Drawing Project.1

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const canvas = document.querySelector("canvas");
 
-// 캔버스에 그림 그릴 때 쓰는거
+// context는 기본적으로 붓(브러쉬)를 의미
 const ctx = canvas.getContext("2d");
 
 canvas.width = 800;
 canvas.height = 800;
 
-//(선을 긋지 않으면서)페인팅 브러쉬의 위치를 옮기고
-ctx.moveTo(100, 100);
+//벽
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
 
-//내가 현재 있는곳으로부터 옮길 곳까지(선을 그려준다)
-//중간중간 작동하는지 보려면 stroke 함수를 호출하여 확인 가능
-ctx.lineTo(200, 100);
-ctx.lineTo(200, 200);
-ctx.lineTo(100, 200);
-ctx.lineTo(100, 100);
+//문을 그려준다.
+ctx.fillRect(300, 300, 50, 100);
 
-//완벽히 사각형을 그리면 fill함수 호출하여 색칠 가능
-ctx.fillStyle = "gold";
+//천장
+ctx.fillRect(200, 200, 200, 20);
+
+//지붕
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100); //벽의 넓이도 생각해서 중앙까지 그린다.
+ctx.lineTo(450, 200);
 ctx.fill();
