808393e641f5a923fb9162e14f5863499f37a77b
Hyojin Kim <hjkim3009@gmail.com>
2022-07-29 23:07:50 +0900

    Filling Mode

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const lineWidth = document.getElementById("line-width");
 const color = document.getElementById("color");
+const modeBtn = document.getElementById("mode-btn");
 
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+let isFilling = false;
 
 function onMove(event) {
   if (isPainting) {
@@ -49,11 +51,28 @@ function onColorClick(event) {
   color.value = event.target.dataset.color;
 }
 
+function onModeClick() {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+}
+
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, 800, 800);
+  }
+}
+
 //그리기
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", onMouseUp);
 canvas.addEventListener("mouseleave", onMouseUp);
+canvas.addEventListener("click", onCanvasClick);
 
 //붓 사이즈
 lineWidth.addEventListener("change", onLineWidthChange);
@@ -61,3 +80,6 @@ color.addEventListener("change", onColorChange);
 
 //팔레트
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+
+//캔버스 채우기
+modeBtn.addEventListener("click", onModeClick);
