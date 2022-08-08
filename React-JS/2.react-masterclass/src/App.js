94fa59e68701787a54b4c38f94ccc50f16dfd267
Hyojina <hjkim3009@gmail.com>
2022-08-09 01:44:52 +0900

    As and Attrs

--- React-JS/2.react-masterclass/src/App.js
+++ React-JS/2.react-masterclass/src/App.js
@@ -5,22 +5,29 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
+// const Btn = styled.button`
+//   color: white;
+//   background-color: black;
+//   border: 0px;
+//   border-radius: 15px;
+// `;
 
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: blue;
 `;
 
 //구현부분
 function App() {
   return (
     <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      {/* <Btn>Log in</Btn>
+      <Btn as="a" href="/">
+        Log in
+      </Btn> */}
     </Father>
   );
 }
