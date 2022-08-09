7a0186c9edd97bd8f16cee57feebf12dd0409c1a
Hyojina <hjkim3009@gmail.com>
2022-08-09 21:10:40 +0900

    Themes

--- React-JS/2.react-masterclass/src/App.js
+++ React-JS/2.react-masterclass/src/App.js
@@ -1,57 +1,22 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
-`;
-
-const rotationAnimation = keyframes`
-  0%{
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50%{
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-  100%{
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 40px;
-`;
-
-const Box = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: chocolate;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotationAnimation} 1s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 60px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji as="p">😊</Emoji>
-      </Box>
-      <Emoji>😘</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
