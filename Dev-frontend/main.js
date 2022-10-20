9fe11cb919d2c72667c4d0f8e55d24e14e8b63b1
Hyojina <hjkim3009@gmail.com>
2022-10-20 19:47:38 +0900

    study: Operator

--- Dev-frontend/main.js
+++ Dev-frontend/main.js
@@ -1,15 +1,54 @@
-import getType from "./getType";
+// -------------- 산술 연산자
 
-console.log(typeof "Hello World!"); //string
-console.log(typeof 123); //number
-console.log(typeof true); //boolean
-console.log(typeof undefined); //undefined
-console.log(typeof null); //object
-console.log(typeof {}); //object
-console.log(typeof []); //object
+console.log(1 + 2); // 3
+console.log(5 - 7); //-2
+console.log(3 * 4); //12
+console.log(8 / 2); //4
+console.log(9 % 6); //3
 
-console.log(getType(123)); //Number
-console.log(getType(false)); //False
-console.log(getType(null)); //Null
-console.log(getType({})); //Object
-console.log(getType([])); //Array
+// -------------- 할당 연산자
+
+let a = 2;
+a += 1; // a = a + 1와 동일
+
+console.log(a); //3
+
+// -------------- 비교 연산자
+
+function isEqual(x, y) {
+  return x === y;
+}
+
+console.log(isEqual(1, 1)); //true
+console.log(isEqual(1, 2)); //false
+console.log(isEqual(2, "2")); //false
+console.log(3 !== 4); //true
+
+console.log(3 < 4); // true
+console.log(3 >= 3); //true
+
+// -------------- 논리 연산자
+
+const b = 1 === 1;
+const c = "AB" === "AB";
+const d = false;
+
+console.log(b);
+console.log(c);
+console.log(d);
+
+console.log(b && d); //false
+console.log(b || d); // true
+console.log(!d); // true
+
+// -------------- 삼항 연산자
+
+const x = 1 < 2; //true
+
+if (a) {
+  console.log("참");
+} else {
+  console.log("거짓");
+}
+
+console.log(a ? "참" : "거짓"); // 위와 동일한 코드
