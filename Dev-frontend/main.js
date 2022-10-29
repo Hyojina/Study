40017cd08ee99c266b34fdd409c02f918dcbd95d
Hyojina <hjkim3009@gmail.com>
2022-10-29 20:38:37 +0900

    study: Arguments Object

--- Dev-frontend/main.js
+++ Dev-frontend/main.js
@@ -1,16 +1,6 @@
-// 형 변환(Type conversion)
-
-// Truthy(참 같은 값)
-// true, {}, [], 1, 2, "false", -12, "3.14" ...
-
-// Falsy(거짓 같은 값)
-// flase, "", null, undefined, 0, -0, NaN
-
-if (false) {
-  console.log(123); // 123 출력 안됨
+function num() {
+  console.log(arguments);
+  return arguments[0] + arguments[1]; // 3
 }
 
-if ("flase") {
-  //(그저 문자데이터!)
-  console.log(456); // 456 출력됨
-}
+console.log(num(1, 2));
