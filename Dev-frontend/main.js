3f6ffe5eb567c4b2dd78ce338d542c59fe10620b
Hyojina <hjkim3009@gmail.com>
2022-11-01 22:19:22 +0900

    study: Timer Function

--- Dev-frontend/main.js
+++ Dev-frontend/main.js
@@ -1,10 +1,12 @@
-// 호이스팅(Hoisting)
-// 함수 선언부가 유효범위 최상단으로 끌어올려지는 현상
+// 타이머 함수
+// setTimeout(함수, 시간): 일정 시간 후 함수 실행
+// setInterval(함수, 시간): 시간 간격마다 함수 실행
+// clearTimeout(): 설정된 Timeout 함수를 종료
+// clearInterval(): 설정된 Interval 함수를 종료
 
-const a = 7;
+const timer = setInterval(() => console.log("Hyojina!"), 1000);
 
-double();
+console.log(timer);
 
-function double() {
-  console.log(a * 2);
-}
+const h1El = document.querySelector("h1");
+h1El.addEventListener("click", () => clearInterval(timer));
