fc158c4e4adb0e4f2ff86324733b7f31fc78db50
Hyojina <hjkim3009@gmail.com>
2022-11-06 00:02:34 +0900

    study: Prototype

--- Dev-frontend/main.js
+++ Dev-frontend/main.js
@@ -1,12 +1,12 @@
-// 콜백(Callback)
-
-// 매개변수를 하나 추가해줍니다. callback 혹은 cb
-function timeout(cb) {
-  setTimeout(() => {
-    console.log("타이머");
-    cb(); // 매개변수로 넘어온 함수를 호출합니다.
-  }, 3000);
+function User(first, last) {
+  this.firstName = first;
+  this.lastName = last;
 }
+User.prototype.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
 
-// timout 함수에 인수를 넣는 형태입니다.
-timeout(() => console.log("타이머 끝!"));
+const hyojina = new User("Hyojina", "Kim");
+
+console.log(hyojina);
+console.log(hyojina.getFullName());
