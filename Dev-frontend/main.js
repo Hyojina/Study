5bb346b7434640dc64a1723a37dee7c45f6c4e7a
Hyojina <hjkim3009@gmail.com>
2022-11-06 22:43:22 +0900

    study: Extends

--- Dev-frontend/main.js
+++ Dev-frontend/main.js
@@ -1,16 +1,29 @@
-const timer = {
-  name: "Hyojina",
-  timeoutArrow: function () {
-    setTimeout(() => {
-      console.log(this.name);
-    }, 2000);
-  },
-  timeoutNormal: function () {
-    setTimeout(function () {
-      console.log(this.name);
-    }, 2000);
-  },
-};
+class Vehicle {
+  constructor(name, wheel) {
+    this.name = name;
+    this.wheel = wheel;
+  }
+}
+const myVehicle = new Vehicle("운송수단", 2);
+console.log(myVehicle);
 
-timer.timeoutArrow(); // Hyojina
-timer.timeoutNormal(); //
+class Bicycle extends Vehicle {
+  constructor(name, wheel) {
+    super(name, wheel);
+  }
+}
+const myBicycle = new Bicycle("삼천리", 2);
+const daughterBicycle = new Bicycle("세발", 3);
+console.log(myBicycle);
+console.log(daughterBicycle);
+
+class Car extends Vehicle {
+  constructor(name, wheel, license) {
+    super(name, wheel);
+    this.license = license;
+  }
+}
+const myCar = new Car("벤츠", 4, true);
+const daughterCar = new Car("포르쉐", 4, false);
+console.log(myCar);
+console.log(daughterCar);
