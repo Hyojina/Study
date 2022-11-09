dc27e394558e975f3e60011bc9f19ba4075c2c20
Hyojina <hjkim3009@gmail.com>
2022-11-09 23:21:32 +0900

    study: Array

--- Dev-frontend/main.js
+++ Dev-frontend/main.js
@@ -1,13 +1,27 @@
-console.log("abs: ", Math.abs(-12)); // 12
+const numbers = [1, 2, 3, 4];
+const fruits = ["Apple", "Banana", "Cherry"];
 
-console.log("min: ", Math.min(2, 8)); // 2
+console.log(numbers[1]); // 2
+console.log(fruits[2]); // Cherry
 
-console.log("max: ", Math.max(2, 8)); // 8
+console.log(numbers.length); // 4
+console.log(fruits.length); // 3
+console.log([4, 5].length); // 2
+console.log([].length); // 0, 배열이 비어있음을 나타냅니다. 자주 사용해요!
 
-console.log("ceil: ", Math.ceil(3.14)); // 4
+console.log(numbers.concat(fruits)); // [1, 2, 3, 4, 'Apple', 'Banana', 'Cherry']
+console.log(numbers); // [1, 2, 3, 4]
+console.log(fruits); // ['Apple', 'Banana', 'Cherry']
 
-console.log("floor: ", Math.floor(3.14)); // 3
+console.log(
+  fruits.forEach(function (element, index, array) {
+    console.log(element, index, array);
+  })
+);
+// Apple 0 (3) ['Apple', 'Banana', 'Cherry']
+// Banana 1 (3) ['Apple', 'Banana', 'Cherry']
+// Cherry 2 (3) ['Apple', 'Banana', 'Cherry']
 
-console.log("round: ", Math.round(3.14)); // 3
+const arr = [5, 10, 26, 44, 70];
 
-console.log("random: ", Math.random()); // 랜덤! 0.22990596352494297
+console.log(arr.find((element) => element > 10)); // 26
