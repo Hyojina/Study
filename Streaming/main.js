a65155b3b3a7d1fe280e6eb0eed2f7dc96f28c7e
Hyojina <hjkim3009@gmail.com>
2022-12-07 21:55:13 +0900

    study

--- Streaming/main.js
+++ Streaming/main.js
@@ -1,12 +1,12 @@
-function solution(s, c) {
-  let answer = 0;
-
-  for (i = 0; i < s.length; i++) {
-    // if (s[i].includes(c)) answer++;
-    // 함수 남발은 좋지 않습니다. 호출 시간이 걸리기 때문!
-    if (s[i] === c) answer++;
-  }
-  return answer;
+function solution(n, edge) {
+  let answer = n;
 }
 
-console.log(solution("COMPUTERPROGRAMMING", "R"));
+console.log(
+  solution(7, [
+    [1, 2],
+    [2, 3],
+    [1, 4],
+    [1, 5],
+  ])
+);
